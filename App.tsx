7d25b268314887c8ae1b2791e0c018ccc08e8ee1
Satyam Kumar <satyamkr8280@gmail.com>
2026-03-04 19:22:41 +0530

    Implement NativeWind to use tailwind css

--- App.tsx
+++ App.tsx
@@ -1,66 +1,12 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  useColorScheme,
-} from 'react-native';
+import "./global.css"
+import { Text, View } from "react-native";
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaView
-      style={[
-        styles.container,
-        { backgroundColor: isDarkMode ? '#121212' : '#ffffff' },
-      ]}>
-
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-      />
-
-      <View style={styles.content}>
-        <Text
-          style={[
-            styles.title,
-            { color: isDarkMode ? '#ffffff' : '#000000' },
-          ]}>
-          🚀 Welcome Satyam
-        </Text>
-
-        <Text
-          style={[
-            styles.subtitle,
-            { color: isDarkMode ? '#cccccc' : '#555555' },
-          ]}>
-          Your React Native project is ready.
-        </Text>
-      </View>
-
-    </SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: '700',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-  },
-});
-
-export default App;
+}
